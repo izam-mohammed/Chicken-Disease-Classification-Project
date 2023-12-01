4468818965e73008e49cffa12953e078e291b351
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-01 21:24:57 +0530

    compleated the modular implementation of prepare base model

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,22 +1,23 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig)
 
 class CofigurationManager:
     def __init__(
         self,
         config_filepath = CONFIG_FILE_PATH,
-        prarms_filepath = PARAMS_FILE_PATH
+        params_filepath = PARAMS_FILE_PATH
         ):
         
         self.config = read_yaml(config_filepath)
-        self.prams = read_yaml(prarms_filepath)
+        self.params = read_yaml(params_filepath)
         
         create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         """
-        return data in the format of dataingestionconfig from config.yaml file 
+        return data in the format of DataIngestionConfig from config.yaml file 
         """
         config = self.config.data_ingestion
 
@@ -30,4 +31,25 @@ class CofigurationManager:
         )
 
         return data_ingestion_config
-      
+
+  
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        """
+        returns the data as PrepareBaseModelConfig data
+        """
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
